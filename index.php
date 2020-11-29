a66811e05c87f9f36b6481674396a0084c4b2c38
mastro-elfo <francesco.209@gmail.com>
2020-11-29 10:34:59 +0100

    Set session cookie params

--- index.php
+++ index.php
@@ -1,8 +1,14 @@
 <?php
 // Start session
+session_set_cookie_params([
+  "SameSite" => "Strict",
+  "Secure" => "true"
+]);
 session_start();
 // Require libs
 require "vendor/autoload.php";
+// Load config file
+$config = parse_ini_file("config.ini");
 // Require resources
 require "resources/api.php";
 require "resources/cors.php";
