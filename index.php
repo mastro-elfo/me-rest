554a036a5b6a202ed8626210fd0d5a1a75316e8c
mastro-elfo <francesco.209@gmail.com>
2021-01-30 13:06:39 +0100

    Require RedBean before the rest

--- index.php
+++ index.php
@@ -1,20 +1,21 @@
 <?php
 // Start session
 session_set_cookie_params([
-  "SameSite" => "Strict",
-  "Secure"   => "true",
+    "SameSite" => "Strict",
+    "Secure"   => "true",
 ]);
 session_start();
 // Require libs
 require_once "vendor/autoload.php";
 // Load config file
 $config = parse_ini_file("config.ini");
+// Require before the rest
+require_once "resources/rb.php";
 // Require resources
 require_once "resources/access.php";
 require_once "resources/api.php";
 require_once "resources/cors.php";
 require_once "resources/log.php";
-require_once "resources/rb.php";
 require_once "resources/sleep.php";
 require_once "resources/status.php";
 require_once "resources/user.php";
