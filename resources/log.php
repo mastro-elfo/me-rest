f58e9ae92c8703e02f3422d329a245b199db130d
mastro-elfo <francesco.209@gmail.com>
2021-01-30 12:47:06 +0100

    Rewrite

--- resources/log.php
+++ resources/log.php
@@ -1,78 +1,96 @@
 <?php
+namespace Log;
 
-// // Define log model
-// class Log extends Model {
-//   public function __construct() {
-//     parent::__construct("Logs", [
-//       // Primary key
-//       "id" => ["INTEGER", "NOT NULL", "PRIMARY KEY", "AUTOINCREMENT", "UNIQUE"],
-//       "datetime" => ["TEXT"],
-//       "url" => ["TEXT"],
-//       "base" => ["TEXT"],
-//       "method" => ["TEXT"],
-//       "referrer" => ["TEXT"],
-//       "ip" => ["TEXT"],
-//       "ajax" => ["INTEGER"],
-//       "scheme" => ["TEXT"],
-//       "user_agent" => ["TEXT"],
-//       "type" => ["TEXT"],
-//       "length" => ["INTEGER"],
-//       "query" => ["BLOB"],
-//       "data" => ["BLOB"],
-//       "cookies" => ["BLOB"],
-//       // "files" => ["BLOB"],
-//       "secure" => ["INTEGER"],
-//       "accept" => ["TEXT"],
-//       "proxy_ip" => ["TEXT"]
-//     ]);
-//   }
-// }
-//
-//
-// Flight::before("start", function(){
-//   $log = get_config("log", true);
-//   // This stops the function but not the next "before"
-//   if(!$log) return true;
-//   //
-//   $model = new Log();
-//   $request = Flight::request();
-//   // Define data object
-//   $data = [
-//     "datetime" => date("Y-m-d H:i:s"),
-//     "url" => $request->url,
-//     "base" => $request->base,
-//     "method" => $request->method,
-//     "referrer" => $request->method,
-//     "ip" => $request->ip,
-//     "ajax" => $request->ajax,
-//     "scheme" => $request->scheme,
-//     "user_agent" =>  $request->user_agent,
-//     "type" =>  $request->type,
-//     "length" =>  $request->length,
-//     "query" =>  $request->query->getData(),
-//     "data" => $request->data->getData(),
-//     "cookies" =>  $request->cookies->getData(),
-//     // "files" => ???
-//     "secure" =>  $request->secure,
-//     "accept" =>  $request->accept,
-//     "proxy_ip" =>  $request->proxy_ip
-//   ];
-//   // Create row
-//   $model->create($data);
-// });
-//
-// Flight::route("GET /api/logs", function(){
-//   // Check access
-//   if(!is_admin()) {
-//     return Flight::stop(UNAUTHORIZED);
-//   }
-//   //
-//   $model = new Log();
-//   $request = Flight::request();
-//   $query = $request->query->getData();
-//   $response = $model->select($query);
-//   // Response
-//   Flight::json($response);
-// });
+require_once "lib/rb.php";
+require_once "resources/user.session.php";
 
-?>
+function create(array $data): int
+{
+    // Create record
+    $log = \R::dispense("log");
+    // Merge data
+    array_like_merge($log, $data);
+    // Save and return
+    return \R::store($log);
+}
+
+function read(integer $id)
+{
+    // Load from db
+    $log = \R::load("log", $id);
+    // If not found
+    if (!$log || $log->id == 0) {
+        return null;
+    }
+
+    // Export
+    $log = $log->export();
+    // Return
+    return $log;
+}
+
+function findAll($offset = 0): array
+{
+    // Query db
+    $logs = \R::findAndExport("log",
+        implode(" ", [
+            "LIMIT :offset, 10",
+        ]), [
+            "offset" => $offset,
+        ]);
+    // Return
+    return $logs;
+}
+
+Flight::before("start", function () {
+    $log = get_config("log", true);
+    // This stops the function but not the next "before"
+    if (!$log) {
+        return true;
+    }
+    // Get request params
+    $request = Flight::request();
+    // Query
+    $id = create([
+        // General info
+        "datetime"   => date("Y-m-d H:i:s"),
+        "user"       => \UserSession\logged_user(),
+        // The following are the request params
+        "url"        => $request->url,
+        "base"       => $request->base,
+        "method"     => $request->method,
+        "referrer"   => $request->method,
+        "ip"         => $request->ip,
+        "ajax"       => $request->ajax,
+        "scheme"     => $request->scheme,
+        "user_agent" => $request->user_agent,
+        "type"       => $request->type,
+        "length"     => $request->length,
+        "query"      => $request->query->getData(),
+        // This is not secure: may contain plain password
+        // "data" => $request->data->getData(),
+        "cookies"    => $request->cookies->getData(),
+        // Can be large
+        // "files" => ???
+        "secure"     => $request->secure,
+        "accept"     => $request->accept,
+        "proxy_ip"   => $request->proxy_ip,
+        "host"       => $request->host,
+    ]);
+});
+//
+Flight::route("GET /api/logs", function () {
+    // Check access
+    if (!is_admin()) {
+        return Flight::stop(UNAUTHORIZED);
+    }
+    // Get query params
+    $request = Flight::request();
+    $params  = $request->query->getData();
+    // Find all
+    $logs = findAll(
+        array_key_exists("offset", $params) ? $params["offset"] : 0
+    );
+    // Response
+    Flight::json($logs);
+});
