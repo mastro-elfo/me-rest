5bcb2557d8932b3998c5b7123327690279b1d132
mastro-elfo <francesco.209@gmail.com>
2021-01-30 12:23:32 +0100

    Remove brackets

--- resources/user.ns.php
+++ resources/user.ns.php
@@ -1,115 +1,115 @@
 <?php
 
-namespace User {
-    require_once "lib/rb.php";
+namespace User;
 
-    function create(array $data): int
-    {
-        // Filter allowed keys
-        $keys = ["password", "username"];
-        $data = allowed_keys($data, $keys);
-        // Map data
-        $maps = ["password" => "hide"];
-        $data = apply_maps($data, $maps);
-        // Create record
-        $user = \R::dispense("user");
-        // Merge data into user
-        array_like_merge($user, $data);
-        // Save
-        return \R::store($user);
-    }
+require_once "lib/rb.php";
 
-    function read(integer $id):  ? object
-    {
-        // Load user from db
-        $user = \R::load("user", $id);
-        // If not found
-        if (!$user || $user->id == 0) {
-            return null;
-        }
-        // Export
-        $user = $user->export();
-        // Filter denied keys
-        $user = denied_keys($user, ["password"]);
-        // Return
-        return $user;
-    }
+function create(array $data): int
+{
+    // Filter allowed keys
+    $keys = ["password", "username"];
+    $data = allowed_keys($data, $keys);
+    // Map data
+    $maps = ["password" => "hide"];
+    $data = apply_maps($data, $maps);
+    // Create record
+    $user = \R::dispense("user");
+    // Merge data into user
+    array_like_merge($user, $data);
+    // Save
+    return \R::store($user);
+}
 
-    function update(integer $id, array $data)
-    {
-        // Filter allowed keys
-        $keys = ["name", "password", "username"];
-        $data = allowed_keys($data, $keys);
-        // Map data
-        $maps = ["password" => "hide"];
-        $data = apply_maps($data, $maps);
-        // Load for update
-        $user = \R::loadForUpdate("user", $id);
-        // Can't update super user
-        if ($user["type"] == "super") {
-            return false;
-        }
-        // Merge data into user
-        array_like_merge($user, $data);
-        // Save
-        return \R::store($user);
-    }
-
-    // Delete from db
-    // TODO: https://redbeanphp.com/api/classes/RedBeanPHP.Facade.html#method_hunt
-    function delete($id)
-    {
-        // Lock row for update
-        $user = \R::loadForUpdate("user", $id);
-        // Can't delete super user
-        if ($user["type"] == "super") {
-            return false;
-        }
-        // Remove
-        \R::trash($user);
-        return true;
-    }
-
-    function login(string $username, string $password) :  ? array
-    {
-        // Query db
-        $user = \R::findOne("user",
-            "username = :username AND password = :password", [
-                "username" => $username,
-                "password" => hide($password),
-            ]);
-        // Check valid user
-        if ($user && $user["id"] != 0) {
-            // User found, export result and return
-            return $user->export();
-        }
-        // User not found: return null
+function read(integer $id):  ? object
+{
+    // Load user from db
+    $user = \R::load("user", $id);
+    // If not found
+    if (!$user || $user->id == 0) {
         return null;
     }
+    // Export
+    $user = $user->export();
+    // Filter denied keys
+    $user = denied_keys($user, ["password"]);
+    // Return
+    return $user;
+}
 
-    function findAll(
-        string $query,
-        $offset = 0,
-        $limit = 10,
-        $super = false) : array
-    {
-        // Query db
-        $users = \R::findAndExport("user",
-            implode(" ", [
-                "username LIKE :query",
-                "LIMIT :offset, :limit",
-            ]), [
-                "query"  => "%$query%",
-                "offset" => $offset,
-                "limit"  => $limit,
-            ]);
-        // Map results
-        $users = array_map(function ($user) {
-            // Remove denied keys
-            $user = denied_keys($user, ["password"]);
-            return $user;
-        }, $users);
-        // Return
-        return $users;
+function update(integer $id, array $data)
+{
+    // Filter allowed keys
+    $keys = ["name", "password", "username"];
+    $data = allowed_keys($data, $keys);
+    // Map data
+    $maps = ["password" => "hide"];
+    $data = apply_maps($data, $maps);
+    // Load for update
+    $user = \R::loadForUpdate("user", $id);
+    // Can't update super user
+    if ($user["type"] == "super") {
+        return false;
     }
+    // Merge data into user
+    array_like_merge($user, $data);
+    // Save
+    return \R::store($user);
+}
+
+// Delete from db
+// TODO: https://redbeanphp.com/api/classes/RedBeanPHP.Facade.html#method_hunt
+function delete($id)
+{
+    // Lock row for update
+    $user = \R::loadForUpdate("user", $id);
+    // Can't delete super user
+    if ($user["type"] == "super") {
+        return false;
+    }
+    // Remove
+    \R::trash($user);
+    return true;
+}
+
+function login(string $username, string $password) :  ? array
+{
+    // Query db
+    $user = \R::findOne("user",
+        "username = :username AND password = :password", [
+            "username" => $username,
+            "password" => hide($password),
+        ]);
+    // Check valid user
+    if ($user && $user["id"] != 0) {
+        // User found, export result and return
+        return $user->export();
+    }
+    // User not found: return null
+    return null;
+}
+
+function findAll(
+    string $query,
+    $offset = 0,
+    $limit = 10,
+    $super = false) : array
+{
+    // Query db
+    $users = \R::findAndExport("user",
+        implode(" ", [
+            "username LIKE :query",
+            "LIMIT :offset, :limit",
+        ]), [
+            "query"  => "%$query%",
+            "offset" => $offset,
+            "limit"  => $limit,
+        ]);
+    // Map results
+    $users = array_map(function ($user) {
+        // Remove denied keys
+        $user = denied_keys($user, ["password"]);
+        return $user;
+    }, $users);
+    // Return
+    return $users;
 }
